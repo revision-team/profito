57d3bf4a08c5b529f63130d7e43e961fbcdab304
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2020-06-10 15:19:22 -0500

    feat(chatCard): Add chatcards to chatSidebar Update chats.tsx, chat.tsx Add badgeAvatar

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,5 +1,57 @@
 import React from "react";
+import BadgeAvatar from "components/chat/badgeavatar";
 
-export default function Chat() {
-  return <div></div>;
+import { Theme, makeStyles } from "@material-ui/core/styles";
+import { Typography, Card, CardHeader, IconButton } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    maxWidth: 350,
+    borderColor: "transparent",
+    boxShadow: "none",
+  },
+  action: {
+    marginTop: 0,
+  },
+}));
+
+export interface User {
+  name: string;
+  avatar: string;
+  caption: string;
+  online: boolean;
+}
+
+interface ChatProps {
+  user: User;
+}
+
+export default function Chat(props: ChatProps) {
+  const classes = useStyles();
+
+  const truncate = (s: string, n: number) => {
+    if (s.length <= n) {
+      return s;
+    }
+    return s.substring(0, 20) + "...";
+  };
+
+  const { user } = props;
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <BadgeAvatar src={user.avatar} alt={user.name} online={user.online} />
+        }
+        action={
+          <IconButton aria-label="settings" className={classes.action}>
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={user.name}
+        subheader={truncate(user.caption, 20)}
+      />
+    </Card>
+  );
 }
--- src/layouts/app/chats.tsx
+++ src/layouts/app/chats.tsx
@@ -5,6 +5,48 @@ import { Scrollbars } from "react-custom-scrollbars";
 import CloseIcon from "@material-ui/icons/Close";
 import { Store } from "store";
 import { ToggleShowChat } from "store/actions";
+import Chat from "components/chat/chat";
+import SearchBar from "material-ui-search-bar";
+import { User } from "components/chat/chat";
+
+const users: User[] = [
+  {
+    name: "Emily Mars",
+    avatar: "/avatars/1.png",
+    caption: "Yep! I saw it!",
+    online: true,
+  },
+  {
+    name: "Sue Fidalgo",
+    avatar: "/avatars/2.png",
+    caption: "",
+    online: false,
+  },
+  {
+    name: "Karen Cats",
+    avatar: "/avatars/3.png",
+    caption: "Maybe, what do you think about it?",
+    online: false,
+  },
+  {
+    name: "Daniel Hans",
+    avatar: "/avatars/4.png",
+    caption: "Somewhere, I don't know",
+    online: true,
+  },
+  {
+    name: "Trin Mabble",
+    avatar: "/avatars/5.png",
+    caption: "",
+    online: false,
+  },
+  {
+    name: "Paula Jacobs",
+    avatar: "/avatars/6.png",
+    caption: "You are doing well, congratulations!",
+    online: true,
+  },
+];
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -33,10 +75,19 @@ export default function Chats() {
       {state.showChat && (
         <div className={classes.container}>
           <div className={classes.search}>
+            {/* <SearchBar
+              onChange={() => console.log("onChange")}
+              onRequestSearch={() => console.log("onRequestSearch")}
+              style={{
+                margin: "0 auto",
+                maxWidth: 800,
+                width: "80%",
+              }}
+            /> */}
             <IconButton
-              edge='start'
-              color='inherit'
-              aria-label='menu'
+              edge="start"
+              color="inherit"
+              aria-label="menu"
               onClick={handleToggleShowChat}
             >
               <CloseIcon />
@@ -45,13 +96,8 @@ export default function Chats() {
           <Divider />
           <div className={classes.board}>
             <Scrollbars autoHide universal renderThumbVertical={ScrollVertical}>
-              {[...new Array(50)].map((e, k) => (
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Dolore omnis harum esse iure totam deleniti? Nostrum,
-                  repellat, veritatis aut officia ipsam pariatur commodi natus
-                  blanditiis repellendus eaque distinctio, est dicta!
-                </p>
+              {users.map((u) => (
+                <Chat user={u} />
               ))}
             </Scrollbars>
           </div>
